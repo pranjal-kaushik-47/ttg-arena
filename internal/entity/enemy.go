25caceb58c48908c7cfbbe56cd8c135d71636960
pranjal-kaushik-47 <pk.kaushik.47007@gmail.com>
2024-09-27 00:24:26 +0530

    chaser movement fix

--- internal/entity/enemy.go
+++ internal/entity/enemy.go
@@ -56,46 +56,99 @@ func (e *Enemy) Reset(env *Environment, metaData *common.GameMetaData) error {
 func GetMovementDirection(enemy *Enemy, player *Player, env *Environment, screenWidth, screenHeight int) (float64, float64) {
 	var x, y float64
 	if player.Sprite.PosX <= enemy.Sprite.PosX {
-		if enemy.Sprite.PosX <= float64(screenWidth) && !env.Colliding("enemy", enemy.Sprite.BoundingBoxShiftRight(2)) {
-			enemy.Sprite.BoundingBox.Vertices = enemy.Sprite.BoundingBoxShiftRight(enemy.Speed).Vertices
-			x = enemy.Speed
+		if enemy.Type == 0 {
+			if enemy.Sprite.PosX <= float64(screenWidth) && !env.Colliding("enemy", enemy.Sprite.BoundingBoxShiftRight(2)) {
+				enemy.Sprite.BoundingBox.Vertices = enemy.Sprite.BoundingBoxShiftRight(enemy.Speed).Vertices
+				x = enemy.Speed
+			} else {
+				x = 0
+			}
 		} else {
-			x = 0
+			if enemy.Sprite.PosX <= float64(screenWidth) && !env.Colliding("enemy", enemy.Sprite.BoundingBoxShiftRight(2)) {
+				enemy.Sprite.BoundingBox.Vertices = enemy.Sprite.BoundingBoxShiftLeft(enemy.Speed).Vertices
+				x = -1 * enemy.Speed
+			} else {
+				x = 0
+			}
 		}
 	} else {
-		if enemy.Sprite.PosX >= 0 && !env.Colliding("enemy", enemy.Sprite.BoundingBoxShiftLeft(2)) {
-			enemy.Sprite.BoundingBox.Vertices = enemy.Sprite.BoundingBoxShiftLeft(enemy.Speed).Vertices
-			x = -1 * enemy.Speed
+		if enemy.Type == 0 {
+			if enemy.Sprite.PosX >= 0 && !env.Colliding("enemy", enemy.Sprite.BoundingBoxShiftLeft(2)) {
+				enemy.Sprite.BoundingBox.Vertices = enemy.Sprite.BoundingBoxShiftLeft(enemy.Speed).Vertices
+				x = -1 * enemy.Speed
+			} else {
+				x = 0
+			}
 		} else {
-			x = 0
+			if enemy.Sprite.PosX >= 0 && !env.Colliding("enemy", enemy.Sprite.BoundingBoxShiftLeft(2)) {
+				enemy.Sprite.BoundingBox.Vertices = enemy.Sprite.BoundingBoxShiftRight(enemy.Speed).Vertices
+				x = enemy.Speed
+			} else {
+				x = 0
+			}
 		}
 	}
 	if player.Sprite.PosY <= enemy.Sprite.PosY {
-		if enemy.Sprite.PosY <= float64(screenHeight) && !env.Colliding("enemy", enemy.Sprite.BoundingBoxShiftDown(2)) {
-			enemy.Sprite.BoundingBox.Vertices = enemy.Sprite.BoundingBoxShiftDown(enemy.Speed).Vertices
-			y = enemy.Speed
+		if enemy.Type == 0 {
+			if enemy.Sprite.PosY <= float64(screenHeight) && !env.Colliding("enemy", enemy.Sprite.BoundingBoxShiftDown(2)) {
+				enemy.Sprite.BoundingBox.Vertices = enemy.Sprite.BoundingBoxShiftDown(enemy.Speed).Vertices
+				y = enemy.Speed
+			} else {
+				y = 0
+			}
 		} else {
-			y = 0
+			if enemy.Sprite.PosY <= float64(screenHeight) && !env.Colliding("enemy", enemy.Sprite.BoundingBoxShiftDown(2)) {
+				enemy.Sprite.BoundingBox.Vertices = enemy.Sprite.BoundingBoxShiftUp(enemy.Speed).Vertices
+				y = -1 * enemy.Speed
+			} else {
+				y = 0
+			}
 		}
 	} else {
-		if enemy.Sprite.PosY >= 0 && !env.Colliding("enemy", enemy.Sprite.BoundingBoxShiftUp(2)) {
-			enemy.Sprite.BoundingBox.Vertices = enemy.Sprite.BoundingBoxShiftUp(enemy.Speed).Vertices
-			y = -1 * enemy.Speed
+		if enemy.Type == 0 {
+			if enemy.Sprite.PosY >= 0 && !env.Colliding("enemy", enemy.Sprite.BoundingBoxShiftUp(2)) {
+				enemy.Sprite.BoundingBox.Vertices = enemy.Sprite.BoundingBoxShiftUp(enemy.Speed).Vertices
+				y = -1 * enemy.Speed
+			} else {
+				y = 0
+			}
 		} else {
-			y = 0
+			if enemy.Sprite.PosY >= 0 && !env.Colliding("enemy", enemy.Sprite.BoundingBoxShiftUp(2)) {
+				enemy.Sprite.BoundingBox.Vertices = enemy.Sprite.BoundingBoxShiftDown(enemy.Speed).Vertices
+				y = enemy.Speed
+			} else {
+				y = 0
+			}
 		}
 	}
 	if x == 0 && y == 0 {
 		screenCenterX, screenCenterY := float64(screenWidth/2), float64(screenHeight/2)
 		if screenCenterX > enemy.Sprite.PosX {
-			x = 1
+			if enemy.Type == 0 {
+				x = 1
+			} else {
+				x = -1
+			}
+
 		} else {
-			x = -1
+			if enemy.Type == 0 {
+				x = -1
+			} else {
+				x = 1
+			}
 		}
 		if screenCenterY > enemy.Sprite.PosY {
-			y = 1
+			if enemy.Type == 0 {
+				y = 1
+			} else {
+				y = -1
+			}
 		} else {
-			y = -1
+			if enemy.Type == 0 {
+				y = -1
+			} else {
+				y = 1
+			}
 		}
 	}
 	return x, y
@@ -108,13 +161,9 @@ func (e *Enemy) RunnerUpdate(metaData *common.GameMetaData, p *Player, env *Envi
 	distanceFromPlayer := Distance(&Point{X: e.Sprite.PosX, Y: e.Sprite.PosY}, &Point{X: p.Sprite.PosX, Y: p.Sprite.PosY})
 	if distanceFromPlayer <= e.Eyesight {
 		x, y := GetMovementDirection(e, p, env, screenWidth, screenHeight)
-		if e.Type == 0 {
-			e.Sprite.PosX += x
-			e.Sprite.PosY += y
-		} else {
-			e.Sprite.PosX -= x
-			e.Sprite.PosY -= y
-		}
+
+		e.Sprite.PosX += x
+		e.Sprite.PosY += y
 
 		e.Sprite.CachedData = CachedData{
 			PosX: e.Sprite.PosX,
